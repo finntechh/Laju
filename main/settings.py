d69cab7f0ddb3e2e62915726b7bb0696a3a62df6
Finn <fhertsch235@gmail.com>
2024-03-09 12:11:32 +0100

    setup

--- main/settings.py
+++ main/settings.py
@@ -61,7 +61,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'templates'
+            'templates'
             ],
         'APP_DIRS': True,
         'OPTIONS': {
